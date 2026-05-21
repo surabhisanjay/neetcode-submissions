d0733d1085fd3c77c1a240f57a7d59b4a7e1d0f3
surabhisanjay <58886405+surabhisanjay@users.noreply.github.com>
2026-05-21 17:56:33 +0530

    Add: hand-of-straights - submission-6

---
+++ Data Structures & Algorithms/hand-of-straights/submission-6.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        nums = {}
+        curmin = float('inf')
+        curmax = 0
+        
+        for i in hand:
+            if i in nums:
+                nums[i] += 1
+            else:
+                nums[i] = 1
+            
+        avail = set(nums.keys())
+        
+        while(avail):
+            currmin = min(avail)
+            i = 0
+            while(i < groupSize and currmin+i in avail):
+                nums[currmin+i] -= 1
+                if nums[currmin+i] == 0:
+                    avail.remove(currmin+i)
+                i += 1
+            if i <= groupSize -1:
+                return False
+        return True
+                
